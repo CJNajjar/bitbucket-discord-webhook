9bd75672abaa2fea52df70073703c7fa22f8fefb
Peter Lohse <peterlohse@yahoo.de>
2018-01-02 11:15:39 +0100

    IP Check nun korrekt

--- webhook.php
+++ webhook.php
@@ -3,7 +3,23 @@
 // edit discord channel -> webhooks -> edit or create webhook -> webhook url
 $webhookUrl = "https://discordapp.com/api/webhooks/..../...";
 
-if (in_array(array("34.198.203.127", "34.198.178.64", "34.198.32.85"), $_SERVER['REMOTE_ADDR']))
+// function source: https://gist.github.com/tott/7684443
+function ip_in_range( $ip, $range )
+{
+	if ( strpos( $range, '/' ) == false )
+	{
+		$range .= '/32';
+	}
+	// $range is in IP/CIDR format eg 127.0.0.1/24
+	list( $range, $netmask ) = explode( '/', $range, 2 );
+	$range_decimal = ip2long( $range );
+	$ip_decimal = ip2long( $ip );
+	$wildcard_decimal = pow( 2, ( 32 - $netmask ) ) - 1;
+	$netmask_decimal = ~ $wildcard_decimal;
+	return ( ( $ip_decimal & $netmask_decimal ) == ( $range_decimal & $netmask_decimal ) );
+}
+
+if (!in_array($_SERVER['REMOTE_ADDR'], array("34.198.203.127", "34.198.178.64", "34.198.32.85")) && !ip_in_range($_SERVER['REMOTE_ADDR'], "104.192.136.0/21"))
 {
 	die();
 }
